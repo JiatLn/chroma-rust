fc5ab1e8094e71f8789288c18eb2faf568070405
cx33 <lne128gene980@163.com>
2022-11-13 23:53:53 +0800

    feat(valid): improve valid hex string

--- src/utils/valid.rs
+++ src/utils/valid.rs
@@ -1,7 +1,22 @@
 /// checking if a color can be parsed by chroma-rust
+///
+/// you can use `chroma_rust::valid` to try if a color argument can be correctly parsed as color by `chroma_rust`.
+///
+/// ```rust
+/// chroma_rust::valid("red");
+/// chroma_rust::valid("bread");
+/// chroma_rust::valid("#F0000D");
+/// chroma_rust::valid("#FOOOOD");
+/// ```
 pub fn valid(str: &str) -> bool {
     let valid = match str {
-        str if str.starts_with("#") => true,
+        str if str.starts_with("#") => {
+            if str.len() == 4 || str.len() == 7 {
+                str.chars().skip(1).all(|c| c.is_ascii_hexdigit())
+            } else {
+                false
+            }
+        }
         str if str.starts_with("rgba") => true,
         str if str.starts_with("rgb") => true,
         str if str.starts_with("lab") => true,
@@ -21,6 +36,21 @@ mod tests {
     #[test]
     fn test_valid_hex() {
         assert!(valid("#abcdef"));
+    }
+
+    #[test]
+    fn test_valid_hex_short() {
+        assert!(valid("#abc"));
+    }
+
+    #[test]
+    fn test_valid_hex_invalid() {
+        assert!(!valid("#FOOOOD"));
+    }
+
+    #[test]
+    fn test_valid_hex_invalid_length() {
+        assert!(!valid("#abcde"));
     }
 
     #[test]
