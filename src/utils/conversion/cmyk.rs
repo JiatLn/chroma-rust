97e7678ff5844c8e2c87723409377ae4f3f303e1
林佳涛 <lne128gene980@163.com>
2022-11-28 11:26:08 +0800

    refactor(conversion): cmyk

--- src/utils/conversion/cmyk.rs
+++ src/utils/conversion/cmyk.rs
@@ -6,33 +6,17 @@ pub fn rgb2cmyk(color: (u8, u8, u8)) -> (f64, f64, f64, f64) {
     let g = g as f64 / 255.0;
     let b = b as f64 / 255.0;
     let k = 1.0 - f64::max(f64::max(r, g), b);
-    let f = if k < 1. { 1. / (1. - k) } else { 0. };
-    let c = (1.0 - r - k) * f;
-    let m = (1.0 - g - k) * f;
-    let y = (1.0 - b - k) * f;
+    let c = (1.0 - r - k) * (1. - k);
+    let m = (1.0 - g - k) * (1. - k);
+    let y = (1.0 - b - k) * (1. - k);
     (round(c, 2), round(m, 2), round(y, 2), round(k, 2))
 }
 
 pub fn cmyk2rgb(color: (f64, f64, f64, f64)) -> (u8, u8, u8) {
     let (c, m, y, k) = color;
-    if k == 1. {
-        return (0, 0, 0);
-    }
-    let r = if c >= 1. {
-        0.
-    } else {
-        (1. - c) * (1. - k) * 255.
-    };
-    let g = if m >= 1. {
-        0.
-    } else {
-        (1. - m) * (1. - k) * 255.
-    };
-    let b = if y >= 1. {
-        0.
-    } else {
-        (1. - y) * (1. - k) * 255.
-    };
+    let r = (1. - c) * (1. - k) * 255.;
+    let g = (1. - m) * (1. - k) * 255.;
+    let b = (1. - y) * (1. - k) * 255.;
     (r.round() as u8, g.round() as u8, b.round() as u8)
 }
 
